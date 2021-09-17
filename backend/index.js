f14e99712526b1f65732df855f3d6e375261412d
ab <ab@pop-os.localdomain>
2021-09-17 00:17:58 -0700

    packages & eslint added.

--- backend/index.js
+++ backend/index.js
@@ -3,26 +3,23 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-
-const mysql = require("mysql");
+const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    user: "root",
-    host: "ubereatsdb.cauvszlanaze.us-east-2.rds.amazonaws.com",
-    password: "12345678",
-    database: "sys",
-    port: "3306",
-    multipleStatements: true
-  });
+  user: 'root',
+  host: 'ubereatsdb.cauvszlanaze.us-east-2.rds.amazonaws.com',
+  password: '12345678',
+  database: 'sys',
+  port: '3306',
+  multipleStatements: true,
+});
 
-db.connect((err)=>{
-  if(err) {
-    throw 'Error occured' + err;
+db.connect((err) => {
+  if (err) {
+    throw new Error(`Error occured ${err}`);
   }
   console.log('connection created');
-})
-
-
+});
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
