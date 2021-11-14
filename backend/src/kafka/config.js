d301fd8e3cb7a4f0f7a953b5b99edcb15c6b49c8
Anil Gubbala <anil.gubbala@sjsu.edu>
2021-11-14 02:17:37 -0800

    kafka config updated

--- backend/src/kafka/config.js
+++ backend/src/kafka/config.js
@@ -1,6 +1,6 @@
 const config = {
-  KAFKA_HOST: "3.144.9.176",
-  KAFKA_PORT: 2181,
+  KAFKA_HOST: "ec2-3-144-9-176.us-east-2.compute.amazonaws.com",
+  KAFKA_PORT: 9092,
 };
 
 module.exports = config;
